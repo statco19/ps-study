036b4e46003b2ebe454be1e343e7375fb8717823
andrew <statco19@gmail.com>
2024-05-13 21:23:36 +0900

    created a template

---
+++ a.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int tc = 1;
+
+void sol() {
+	
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	for(int i=1; i<=tc; ++i) {
+		sol();
+	}
+
+	return 0;
+}
