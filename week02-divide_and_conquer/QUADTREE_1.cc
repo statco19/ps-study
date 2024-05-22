b67dba5a3410f21038688bc814bc6f75ece05e9a
andrew <statco19@gmail.com>
2024-05-22 22:56:07 +0900

    created QUADTREE_1.cc

---
+++ week02-divide_and_conquer/QUADTREE_1.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int tc = 1;
+
+string s;
+
+string reverse(string::iterator& it) {
+	char head = *it;
+	++it;
+
+	if(head == 'w' || head == 'b') {
+		return string(1,head);
+	}
+
+	string upperLeft = reverse(it);
+	string upperRight = reverse(it);
+	string lowerLeft = reverse(it);
+	string lowerRight = reverse(it);
+
+	return string("x") + lowerLeft + lowerRight + upperLeft + upperRight;
+}
+
+void sol() {
+	cin >> s;
+	
+	string::iterator it = s.begin();
+	cout << reverse(it) << "\n";
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> tc;
+	for(int i=1; i<=tc; ++i) {
+		sol();
+	}
+
+	return 0;
+}
